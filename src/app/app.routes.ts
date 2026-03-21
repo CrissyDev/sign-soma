ee346fc54e7e0c6a1d23c675c3e5c680209a933b
CrissyDev <christalriziki@gmail.com>
2026-03-22 00:10:07 +0300

    additions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,14 @@
 import { Routes } from '@angular/router';
 import { OnboardingComponent } from './pages/onboarding/onboarding';
-// import { HomeComponent } from './pages/home/home.component';
-// import { CameraComponent } from './pages/camera/camera.component';
-// import { SymptomsComponent } from './pages/symptoms/symptoms.component';
-// import { AccountComponent } from './pages/account/account.component';
+import { Auth } from './pages/auth/auth';
+import { Login } from './pages/login/login';
+import { DoctorDashboard } from './pages/doctor-dashboard/doctor-dashboard';
+import { PatientDashboard } from './pages/patient-dashboard/patient-dashboard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'onboarding', pathMatch: 'full' },
-
-  { path: 'onboarding', component: OnboardingComponent },
-
-//   { path: 'home', component: HomeComponent },
-//   { path: 'camera', component: CameraComponent },
-//   { path: 'symptoms', component: SymptomsComponent },
-//   { path: 'account', component: AccountComponent },
-
-  { path: '**', redirectTo: 'onboarding' }
+  { path: '', component: OnboardingComponent },
+  { path: 'auth', component: Auth },
+  { path: 'login', component: Login },
+  { path: 'doctor-dashboard', component: DoctorDashboard },
+  { path: 'patient-dashboard', component: PatientDashboard },
 ];
