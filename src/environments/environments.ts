08a04e595ecd67da83ad55b18a8e7d08f4593acc
CrissyDev <christalriziki@gmail.com>
2026-04-04 03:54:14 +0300

    started working on the backend with firebase

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,15 @@
 export const environment = {
     production: false,
-    GEMINI_API_KEY: 'AIzaSyBfvgf8lkIwOij96Teg1Zj4ye0QY7ebPcc'
+    GEMINI_API_KEY: 'AIzaSyBfvgf8lkIwOij96Teg1Zj4ye0QY7ebPcc',
+
+    // Firebase Configuration
+  firebase: {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "your-project-id.firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-project-id.appspot.com",
+    messagingSenderId: "1234567890",
+    appId: "1:1234567890:web:abcdef123456",
+    measurementId: "G-ABCDEFGH"
+  }
   };
